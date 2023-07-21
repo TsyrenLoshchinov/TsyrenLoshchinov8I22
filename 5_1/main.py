8ae73f3920d663c27c43b997a13b0cb9be14d90b
[Tsyren Loshchinov 8I22] <[loshchinov@inbox.ru]>
2023-07-22 00:51:25 +0800

    change code

--- 5_1/main.py
+++ 5_1/main.py
@@ -7,11 +7,11 @@ books = [
     ["Судьба Человека", "М.Шолохов", "1956"],
     ["Мцыри", "М.Ю.Лермонтов", "1840"],
 ]
-with open("books.csv","w", encoding="utf-8-sig",  newline='') as file:
+with open("books.csv", "w", encoding="utf-8-sig", newline='') as file:
     writer = csv.writer(file, delimiter=";")
     writer.writerow(z)
 for book in books:
-    with open("books.csv","a", encoding="utf-8-sig",  newline='') as file:
+    with open("books.csv", "a", encoding="utf-8-sig", newline='') as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow(
             book
