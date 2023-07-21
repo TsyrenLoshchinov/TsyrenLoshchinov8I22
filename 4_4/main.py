e29dd1982d1db9cf4d9bbfc436a652e5c8de18a5
[Tsyren Loshchinov 8I22] <[loshchinov@inbox.ru]>
2023-07-22 00:17:55 +0800

    change code

--- 4_4/main.py
+++ 4_4/main.py
@@ -1,14 +1,16 @@
 import itertools
 
-def find_combinations(numbers, target_sum):
+
+def find_combinations(n, t):
     combinations = []
-    for i in range(1, len(numbers)+1):
-        for combo in itertools.combinations(numbers, i):
-            if sum(combo) == target_sum:
+    for i in range(1, len(n)+1):
+        for combo in itertools.combinations(n, i):
+            if sum(combo) == t:
                 combinations.append(combo)
     return set(combinations)
 
+
 numbers = [1, 2, 3, 4, 5]
 target_sum = 9
-combinations = find_combinations(numbers, target_sum)
-print("Уникальные комбинации чисел", numbers, "с суммой", target_sum, ":", combinations)
+c = find_combinations(numbers, target_sum)
+print("Уникальные комбинации чисел", numbers, "с суммой", target_sum, ":", c)
