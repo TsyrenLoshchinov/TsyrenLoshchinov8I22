b3a8435466be01667deb4ae04393d87d959188af
[Tsyren Loshchinov 8I22] <[loshchinov@inbox.ru]>
2023-07-21 22:38:35 +0800

    change code

--- 2_9/main.py
+++ 2_9/main.py
@@ -6,8 +6,8 @@ x = 0
 for i in range(len(k)):
     if int(k[i]) > m:
         m = int(k[i])
-        y = i+1
-x = len(k)-y+1
-print(m," - максимальная цифра")
-print(y," - позиция от начала")
-print(x," - позиция с конца")
+        y = i + 1
+x = len(k) - y + 1
+print(m, " - максимальная цифра")
+print(y, " - позиция от начала")
+print(x, " - позиция с конца")
