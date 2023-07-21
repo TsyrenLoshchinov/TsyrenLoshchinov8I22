619d3ae877b9b35db9ec39bc5a4cf8dec63e4d1b
[Tsyren Loshchinov 8I22] <[loshchinov@inbox.ru]>
2023-07-21 23:58:15 +0800

    change code

--- 3_7/main.py
+++ 3_7/main.py
@@ -1,9 +1,10 @@
 a = str(input("введите строку: "))
-p=dict.fromkeys(a)
+p = dict.fromkeys(a)
 for i in a:
     if i in "уеэоаыяию":
-        p[i] = ("True")
+        p[i] = "True"
     elif i in "цкнгшщзхъждлрпвфчсмтьб":
-        p[i] = ("False")
-    else: del p[i]
+        p[i] = "False"
+    else:
+        del p[i]
 print(p)
