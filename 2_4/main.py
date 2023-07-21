0ad0549ee424309eb2d8028a2fdd13f4a1614ce4
[Tsyren Loshchinov 8I22] <[loshchinov@inbox.ru]>
2023-07-21 22:45:38 +0800

    change code

--- 2_4/main.py
+++ 2_4/main.py
@@ -6,9 +6,9 @@ d = b * b - 4 * a * c
 if d < 0:
     print("Корней не существует")
 elif d == 0:
-    x1 = (-b/(2 * a))
+    x1 = (-b / (2 * a))
     print(x1)
 elif d > 0:
-    x1 = ((-b+pow(d, 0.5))/(2 * a))
+    x1 = ((-b + pow(d, 0.5)) / (2 * a))
     x2 = ((-b - pow(d, 0.5)) / (2 * a))
     print(x1, x2)
