e127acf8d2a402952e15448c68d7fc700266d197
[Tsyren Loshchinov 8I22] <[loshchinov@inbox.ru]>
2023-07-22 00:29:22 +0800

    change code

--- 4_4/main.py
+++ 4_4/main.py
@@ -3,7 +3,7 @@ import itertools
 
 def find_combinations(n, t):
     combinations = []
-    for i in range(1, len(n)+1):
+    for i in range(1, len(n) + 1):
         for combo in itertools.combinations(n, i):
             if sum(combo) == t:
                 combinations.append(combo)
